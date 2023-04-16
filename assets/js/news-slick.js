cb9fccd56627969b792f4f8e25491535f9a6f4cd
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2023-04-17 00:05:29 +0200

    add new section

---
+++ assets/js/news-slick.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".n-slider").slick({
+        // autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+    });
+});
