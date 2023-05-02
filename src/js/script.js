dac4e66442f45912bd409deec910b060804b3137
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2023-05-02 11:01:59 +0200

    fix slick slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 let menuBtn = document.querySelector(".nav__burger");
 let menu = document.querySelector(".menu");
 let submenu = document.querySelector(".submenu");
+let projSubmenu = document.querySelector(".proj__submenu");
 let down = document.querySelector(".aboutus__arrow-down");
+let pDown = document.querySelector(".projects__arrow-down");
 
 menuBtn.addEventListener("click", function () {
     menuBtn.classList.toggle("active");
@@ -9,6 +11,9 @@ menuBtn.addEventListener("click", function () {
 });
 down.addEventListener("click", function () {
     submenu.classList.toggle("submenu__active");
+});
+pDown.addEventListener("click", function () {
+    projSubmenu.classList.toggle("proj__submenu__active");
 });
 
 // window.history.back();
