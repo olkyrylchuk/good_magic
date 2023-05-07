3cc7637ff93fe5478c9d1e36df44f2f8af9b25ca
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2023-05-07 02:21:39 +0200

    add links

--- src/js/copy.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".copy").onclick = (event) => {
-    const elem = event.target;
-    console.log(elem);
-};
--- src/js/script.js
+++ src/js/script.js
@@ -15,24 +15,3 @@ down.addEventListener("click", function () {
 pDown.addEventListener("click", function () {
     projSubmenu.classList.toggle("proj__submenu__active");
 });
-
-// Керування вкладками проєктів
-let all = document.querySelector(".projects__all");
-let active = document.querySelector(".projects__active");
-let finished = document.querySelector(".projects__finished");
-let completed = document.querySelector(".completed");
-let allProjects = document.querySelector(".project__status");
-// const working = document.querySelector(".working");
-let working = document.getElementsByClassName("working");
-
-active.addEventListener("click", function () {
-    // console.log(working);
-    working.List.add("visible");
-
-    // element.classList.add("visible");
-});
-
-// function myFunction() {
-//     var element = document.getElementsByClassName("working");
-//     element.classList.add("visible");
-// }
