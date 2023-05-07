910a636ffac330174724b78d972bd49f89ba3181
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2023-05-07 21:48:45 +0200

    opinian section fixed

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,25 @@ pDown.addEventListener("click", function () {
     projSubmenu.classList.toggle("proj__submenu__active");
     pDown.classList.toggle("rotate");
 });
+
+// блок відповідає за розкріття відгуків
+let resize = document.querySelector(".opinions-text__resize");
+let opinanItemOne = document.querySelector(".opinian__item-01");
+let opinanItemTwo = document.querySelector(".opinian__item-02");
+let opinanItemThree = document.querySelector(".opinian__item-03");
+let opinanItem = document.querySelector(".opinions-block-item-01");
+let opinanItemSecond = document.querySelector(".opinions-block-item-02");
+let opinanItemTherd = document.querySelector(".opinions-block-item-03");
+let opinanHidden = document.querySelector(".opinions-text__hidden");
+
+opinanItem.addEventListener("click", function () {
+    opinanItemOne.classList.toggle("opinions-text__resize");
+});
+
+opinanItemSecond.addEventListener("click", function () {
+    opinanItemTwo.classList.toggle("two-columns-resize");
+});
+
+opinanItemTherd.addEventListener("click", function () {
+    opinanItemThree.classList.toggle("opinions-text__resize");
+});
