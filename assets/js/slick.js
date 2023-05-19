bdb5907881d3d2c0e3afc2ae90b3deffc23fe63c
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2023-05-19 19:11:49 +0200

    slick

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -61,20 +61,20 @@ $(".n-slider").slick({
     ],
 });
 
-$(".news-block").slick({
-    autoplay: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: true,
-    responsive: [
-        {
-            breakpoint: 430,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-            },
-        },
-    ],
-});
+// $(".news-block").slick({
+//     autoplay: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     dots: true,
+//     responsive: [
+//         {
+//             breakpoint: 430,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 arrows: false,
+//             },
+//         },
+//     ],
+// });
