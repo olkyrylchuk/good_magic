b7984fcbb540d33c458568f1ce611110f1456475
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2023-07-12 23:25:14 +0200

    section partners - slider

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -1,3 +1,49 @@
+$(".partners__slider").slick({
+    autoplay: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+                autoplay: true,
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: false,
+            },
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: false,
+            },
+        },
+        {
+            breakpoint: 430,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: false,
+            },
+        },
+    ],
+});
 $(".press__slider").slick({
     autoplay: true,
     slidesToShow: 4,
